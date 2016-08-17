e156f5bc5e5101bc7821d29bc0a9b427543b1d75
Peter Bengtsson <mail@peterbe.com>
2016-08-17 09:52:11 -0400

    finishing touches

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"github.com/coocood/freecache"
 	"github.com/dustin/go-humanize"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/config"
@@ -39,21 +40,28 @@ func GetChildren(url string) (Child, error) {
 	if !strings.Contains(url, "://") {
 		url = "http://" + url
 	}
+	var root Child
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add(
+		"User-Agent",
+		"Mozilla/5.0 (compatible; htmltree/1.0; +https://htmltree.peterbe.com)",
+	)
 	t0 := time.Now()
-	response, err := http.Get(url)
+	response, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return root, err
 	}
 	defer response.Body.Close()
-
+	// t1 := time.Now()
 	doc, err := html.Parse(response.Body)
 	if err != nil {
 		panic(err)
 	}
 	// t1 := time.Now()
-	elapsed := time.Since(t0)
-
-	var root Child
+	t2 := time.Now()
+	// elapsed := time.Since(t0)
 
 	var f func(*html.Node, int, int) []Child
 	f = func(n *html.Node, depth, parentsize int) []Child {
@@ -101,7 +109,7 @@ func GetChildren(url string) (Child, error) {
 				1.0,
 				f(c, 0, size),
 				false,
-				elapsed / time.Millisecond,
+				t2.Sub(t0) / time.Millisecond,
 				humanize.Bytes(uint64(size)),
 			}
 			return root, nil
@@ -131,6 +139,18 @@ func Tree(ctx *iris.Context) {
 			})
 			ctx.SetStatusCode(iris.StatusBadRequest) // 500
 		} else {
+			cacheKey := []byte("recent")
+			got, err := cache.Get(cacheKey)
+			var recent string
+			if err != nil {
+				// never stored before
+				recent = ""
+			} else {
+				// prepare concatenation
+				recent = fmt.Sprintf("|%s", string(got))
+			}
+			recent = fmt.Sprintf("%s%s", url.URL, recent)
+			cache.Set(cacheKey, []byte(recent), 60*60*24*7) // 7 days
 			ctx.JSON(iris.StatusOK, child)
 		}
 	}
@@ -138,25 +158,39 @@ func Tree(ctx *iris.Context) {
 
 func TreeInfo(ctx *iris.Context) {
 	recent := []string{}
+	cacheKey := []byte("recent")
+	if got, err := cache.Get(cacheKey); err == nil {
+		recent = strings.Split(string(got), "|")
+	}
 	ctx.JSON(iris.StatusOK, iris.Map{
 		"jobs":   0,
 		"recent": recent,
 	})
 }
 
+var (
+	debug     bool
+	cache     *freecache.Cache
+	cacheSize = 1024 * 1024 // 1Mb
+)
+
 func Index(ctx *iris.Context) {
-	ctx.ServeFile("./dist/index.html", false)
+	if debug == true {
+		ctx.ServeFile("./client/index.html", false)
+	} else {
+		ctx.ServeFile("./dist/index.html", false)
+	}
 }
 
 func main() {
+	cache = freecache.NewCache(cacheSize)
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	debug := os.Getenv("DEBUG")
+	debug = os.Getenv("DEBUG") == "true" || os.Getenv("DEBUG") == "1"
 	irisConfig := config.Iris{
-		// DisablePathCorrection: true,
-		IsDevelopment: debug == "true",
+		IsDevelopment: debug,
 	}
 	api := iris.New(irisConfig)
 	api.Get("/tree", TreeInfo)
